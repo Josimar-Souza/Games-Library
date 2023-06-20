3532c4d1131dfe65cbc82a8a2653f6a32bc718a4
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-20 12:03:31 -0300

    feat: Added more methods to gamesAPI class

--- src/domain/gamesAPI/index.js
+++ src/domain/gamesAPI/index.js
@@ -10,9 +10,105 @@ class GamesAPI {
     });
   }
 
+  async addNewCategory(category) {
+    try {
+      const { data } = await this.api.post(`${baseURL}/categories`, category);
+
+      return data;
+    } catch (e) {
+      const { data: { message } } = e;
+
+      return ErrorCreator(e, message);
+    }
+  }
+
+  async getAllCategories() {
+    try {
+      const { data } = await this.api.get(`${baseURL}/categories`);
+
+      return data;
+    } catch (e) {
+      const { data: { message } } = e;
+
+      return new ErrorCreator(e, message);
+    }
+  }
+
+  async deleteCategoryById(categoryId) {
+    try {
+      const { data } = await this.api.delete(`${baseURL}/categories/${categoryId}`);
+
+      return data;
+    } catch (e) {
+      const { data: { message } } = e;
+
+      return new ErrorCreator(e, message);
+    }
+  }
+
   async getAllGames() {
     try {
       const { data } = await this.api.get(`${baseURL}/games`);
+
+      return data;
+    } catch (e) {
+      const { data: { message } } = e;
+
+      return new ErrorCreator(e, message);
+    }
+  }
+
+  async getGameById(gameId) {
+    try {
+      const { data } = await this.api.get(`${baseURL}/games/${gameId}`);
+
+      return data;
+    } catch (e) {
+      const { data: { message } } = e;
+
+      return new ErrorCreator(e, message);
+    }
+  }
+
+  async addNewGame(newGame) {
+    try {
+      const { data } = await this.api.post(`${baseURL}/games`, newGame);
+
+      return data;
+    } catch (e) {
+      const { data: { message } } = e;
+
+      return new ErrorCreator(e, message);
+    }
+  }
+
+  async updateGame(newValues, gameId) {
+    try {
+      const { data } = await this.api.put(`${baseURL}/games/${gameId}`, newValues);
+
+      return data;
+    } catch (e) {
+      const { data: { message } } = e;
+
+      return new ErrorCreator(e, message);
+    }
+  }
+
+  async deleteGameById(gameId) {
+    try {
+      const { data } = await this.api.delete(`${baseURL}/games/${gameId}`);
+
+      return data;
+    } catch (e) {
+      const { data: { message } } = e;
+
+      return new ErrorCreator(e, message);
+    }
+  }
+
+  async getGamesByCategory(category) {
+    try {
+      const { data } = await this.api.get(`${baseURL}/games/category/${category}`);
 
       return data;
     } catch (e) {
