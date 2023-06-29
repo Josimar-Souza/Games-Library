de8cafe79619dc6994a836daa14af55f5caefdb6
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-29 11:40:44 -0300

    feat: Added selected style for categories

--- src/components/Sider/index.jsx
+++ src/components/Sider/index.jsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import SiderContainer from './siderStyles';
 import { gamesContext } from '../../context/gamesContext';
 import Button from '../Button';
 
 function Sider() {
   const { categories, searchByCategory } = useContext(gamesContext);
+  const [selectedCategory, setSelectedCategory] = useState('');
 
   const onCategoryClick = (category) => {
+    setSelectedCategory(category);
     searchByCategory(category);
   };
 
@@ -17,6 +19,11 @@ function Sider() {
           key={_id}
           margin="10px 0"
           background="rgba(255, 255, 255, 0.2)"
+          boxShadow={
+            category === selectedCategory
+              ? '0 0 5px blue'
+              : '0 2px 5px rgba(0, 0, 0, 0.3)'
+          }
           border="none"
           onClick={() => onCategoryClick(category)}
         >
