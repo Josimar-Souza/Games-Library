2a41bd45025d3dbcd979aff66e07c07600e1c222
Josimar Souza <contact.josimarsouza@gmail.com>
2023-06-30 10:32:53 -0300

    refactor: Making loading component fixed

--- src/components/Loading/loadingStyles.js
+++ src/components/Loading/loadingStyles.js
@@ -6,10 +6,17 @@ export const SpinContainer = styled.div`
   border-radius: 15px;
   display: flex;
   flex-direction: column;
+  height: ${({ height }) => height};
   justify-content: center;
+  margin: ${({ margin }) => margin};
   min-height: 50px;
   padding: 10px;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
   width: 20%;
+  z-index: 2;
 `;
 
 export const SpinTip = styled.p`
